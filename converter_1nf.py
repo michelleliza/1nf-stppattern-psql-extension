951406d0cbca4cda2e30215a0cb1d08b8d862e9e
13516015 - Michelle Eliza Gananjaya <13516015@std.stei.itb.ac.id>
2020-03-24 15:21:05 +0700

    add converter for n1nf

--- converter_1nf.py
+++ converter_1nf.py
@@ -7,7 +7,6 @@ for i in range(1, 10358):
     lines = f_read.readlines()
     line_1 = lines[0]
     line_1 = line_1.rstrip().split(",")
-    # print(line_1)
     ts_start = line_1[1]
     p_start = 'POINT(' + line_1[2] + ' ' + line_1[3] + ')'
     for j in range(1, len(lines)):
@@ -15,8 +14,7 @@ for i in range(1, 10358):
         ts_end = line[1]
         p_end = 'POINT(' + line[2] + ' ' + line[3] + ')'
         period = '[' + ts_start + ', ' + ts_end + ')'
-        query = "INSERT INTO taxi_trip(taxi_id, p_start, p_end, period) VALUES (" + str(taxi_id) + ", '" + p_start + "', '" + p_end + "', '" + period + "');\n"
+        query = "INSERT INTO taxi_trip(taxi_id, p_start, p_end, time_period) VALUES (" + str(taxi_id) + ", '" + p_start + "', '" + p_end + "', '" + period + "');\n"
         f_write.write(query)
         ts_start = ts_end
         p_start = p_end
-    break
